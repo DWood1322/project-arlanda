5128517220ffca554c9b0e9bb6f6db8e33710609
DWood1322 <echo.arlandanaufal1322@gmail.com>
2023-08-30 14:39:20 +0700

    UPDATE

--- mini-project/resources/views/dataproject.blade.php
+++ mini-project/resources/views/dataproject.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Project</title>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
